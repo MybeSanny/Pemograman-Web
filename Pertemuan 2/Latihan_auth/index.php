f89973916ba1fc2ef08ec4e765a44dd2a0fa5093
Nattzzz <danillooktora81@gmail.com>
2022-10-07 10:41:07 +0700

    Log in

---
+++ Pertemuan 2/Latihan_auth/index.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+//Membuat database / isikan dengan nama kamu
+$database = [
+    'username' => 'nathan',
+    'password' => 'open 123'
+];
+
+//cek apakah terdapat submit yang dilakukan 
+if(isset($_POST['submit'])){
+    //mengambil nilai yang dikirim melalui method phost ke dalam variabel
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // mengecek apakah username dan password sesuai dengan database
+    if ($username == $database['username'] && $password == $database['password']) {
+        // set session username
+        $_SESSION['username'] = $username;
+        // redirrect/mengarahkan menuju halaman admin
+        header("Location:admin.php");
+    } else {
+        // memunculkan pemberitahuan gagal login dan redirrect ke login page
+        echo '<script> alert("Username atau password salah!");
+        window.location="login-page.php"; </script>';
+    }
+} else {
+    // jika tidak ada submit mengembalikan ke login-page
+    echo '<script> window.location="login-page.php" </script>';
+}
+?>
--- Pertemuan 2/session/sesi7_unset.php
+++ Pertemuan 2/session/sesi7_unset.php
@@ -8,7 +8,7 @@ session_start();
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE-edge">
         <meta name="viewport" content="width-device-width, initial-scale=1.0">
-        <title>Sesi 5 - Checking</title>
+        <title>Sesi 7 - unset</title>
     </head>
     <body>
         <?php
